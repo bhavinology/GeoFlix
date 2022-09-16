003ba857ad706f351163e0b79c66804ddefe7b2e
Bhavin <bhavinpatel9624@gmail.com>
2022-09-16 12:56:07 +0530

    fontawesome icons added

---
+++ src/assets/icons/fontAwesome.js
@@ -0,0 +1,49 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHouse,
+  faHome,
+  faCompass,
+  faThumbsUp,
+  faClockRotateLeft,
+  faClock,
+  faUser,
+  faMagnifyingGlass,
+  faEllipsisVertical,
+  faCircleExclamation,
+  faClose,
+  faPlus,
+  faTrash,
+  faShareAlt,
+  faPen,
+  faEye,
+  faEyeSlash,
+  faUpload,
+  faMoon,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(
+  fab,
+  faHouse,
+  faCompass,
+  faThumbsUp,
+  faClockRotateLeft,
+  faClock,
+  faUser,
+  faHome,
+  faMagnifyingGlass,
+  faEllipsisVertical,
+  faCircleExclamation,
+  faClose,
+  faPlus,
+  faTrash,
+  faShareAlt,
+  faPen,
+  faEye,
+  faEyeSlash,
+  faUpload,
+  faMoon,
+  faSun
+);
+
+export { library };

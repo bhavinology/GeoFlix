7e973ab26269676ee53a372531f232d1a0f07915
Bhavin <bhavinpatel9624@gmail.com>
2022-09-16 12:52:38 +0530

    Modified data in db/category and db/videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -1,3 +1,4 @@
+import { v4 as uuid } from "uuid";
 /**
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
@@ -5,17 +6,84 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: uuid(),
+    title: "Importance of Indo-Pacific Region",
+    videoUrl: "https://youtu.be/830sdOcnIWU",
+    category: "Indo-Pacific",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: uuid(),
+    title: "India and the Indo-Pacific",
+    videoUrl: "https://youtu.be/BDKyu4P6q80",
+    category: "Indo-Pacific",
+  },
+  {
+    _id: uuid(),
+    title: "Is AUKUS is the new ASEAN?",
+    videoUrl: "https://youtu.be/KD5QHX9OTfs",
+    category: "Indo-Pacific",
+  },
+  {
+    _id: uuid(),
+    title: "Why Australia ditched France Submarine deal?",
+    videoUrl: "https://youtu.be/9vecxQVYMrc",
+    category: "Indo-Pacific",
+  },
+
+  {
+    _id: uuid(),
+    title: "Middle East geopolitics explained simply",
+    videoUrl: "https://youtu.be/BpTDDKsdMcY",
+    category: "Middle-East",
+  },
+  {
+    _id: uuid(),
+    title: "what are the gulf countries?",
+    videoUrl: "https://youtu.be/-KgEBFocXcw",
+    category: "Middle-East",
+  },
+  {
+    _id: uuid(),
+    title: "Geopolitics of India and Gulf",
+    videoUrl: "https://youtu.be/9ry1XDbzc5c",
+    category: "Middle-East",
+  },
+  {
+    _id: uuid(),
+    title: "Saudi-Qatar conflict",
+    videoUrl: "https://youtu.be/VWS_JfK488Q",
+    category: "Middle-East",
+  },
+  {
+    _id: uuid(),
+    title: "Iran Vs Saudi Arabia: Who Is More Powerful?",
+    videoUrl: "https://youtu.be/xW9jvI2ZAqk",
+    category: "Middle-East",
+  },
+
+  {
+    _id: uuid(),
+    title: "The Quad: Past, Present & Future",
+    videoUrl: "https://youtu.be/EWdpVzo-NoI",
+    category: "Strategic-Groups",
+  },
+  {
+    _id: uuid(),
+    title:
+      "What is Mineral Security Partnership that seeks to end China's dominance ",
+    videoUrl: "https://youtu.be/jgWw0JN5cfA",
+    category: "Strategic-Groups",
+  },
+  {
+    _id: uuid(),
+    title: "I2U2 Summit",
+    videoUrl: "https://youtu.be/3kFDQkOGiv0",
+    category: "Strategic-Groups",
+  },
+  {
+    _id: uuid(),
+    title: " Israel-UAE deal: All about the Abraham Accord",
+    videoUrl: "https://youtu.be/vtPuzVAKeL4",
+    category: "Strategic-Groups",
   },
 ];

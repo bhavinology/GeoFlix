a6710323ee0c626b3a7859c936261efc60d12e54
Bhavin <bhavinpatel9624@gmail.com>
2022-11-23 18:43:26 +0530

    auth context added

---
+++ src/contexts/authContext.js
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [authUser, setAuthUser] = useState(null);
+  const [authToken, setAuthToken] = useState("");
+
+  useEffect(() => {
+    const localStorageAuth = localStorage.getItem("authToken");
+    if (localStorageAuth) {
+      setAuthToken(localStorageAuth);
+      setAuthUser(localStorageAuth.getItem("authUser"));
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ authToken, setAuthToken, authUser, setAuthUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };

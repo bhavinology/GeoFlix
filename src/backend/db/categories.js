99099b0b21542b1b1ff52f76519e47b47065c27f
Bhavin <bhavinpatel9624@gmail.com>
2023-01-03 11:52:38 +0530

    images added in categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,17 +9,25 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "All",
+    imgUrl:
+      "https://res.cloudinary.com/dpcteokzf/image/upload/v1672726601/geoflix/geo_q5sig1.webp",
   },
   {
     _id: uuid(),
     categoryName: "Indo-Pacific",
+    imgUrl:
+      "https://res.cloudinary.com/dpcteokzf/image/upload/v1672725685/geoflix/indo_pacific_nw7rrc.webp",
   },
   {
     _id: uuid(),
     categoryName: "Middle-East",
+    imgUrl:
+      "https://res.cloudinary.com/dpcteokzf/image/upload/v1672726571/geoflix/middleeast_uy33ku.webp",
   },
   {
     _id: uuid(),
     categoryName: "Strategic-Groups",
+    imgUrl:
+      "https://res.cloudinary.com/dpcteokzf/image/upload/v1672726557/geoflix/i2u2_p65yl9.webp",
   },
 ];
